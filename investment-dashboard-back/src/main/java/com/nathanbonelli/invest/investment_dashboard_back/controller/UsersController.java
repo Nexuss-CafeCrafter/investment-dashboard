6de216b4e618900c57eda82e22628137902d4a71
Nathan Bonelli <80414002+Nexuss-CafeCrafter@users.noreply.github.com>
2025-12-16 10:14:27 +0100

    fix: Controller is in fact RestController

--- investment-dashboard-back/src/main/java/com/nathanbonelli/invest/investment_dashboard_back/controller/UsersController.java
+++ investment-dashboard-back/src/main/java/com/nathanbonelli/invest/investment_dashboard_back/controller/UsersController.java
@@ -1,18 +1,18 @@
 package com.nathanbonelli.invest.investment_dashboard_back.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.nathanbonelli.invest.investment_dashboard_back.entity.Users;
 import com.nathanbonelli.invest.investment_dashboard_back.service.UsersService;
 
-@Controller
+@RestController
 @RequestMapping("/api/users")
 public class UsersController {
     @Autowired
